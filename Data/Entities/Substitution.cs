4b58eee15b5dc774c1580073be793844cb3d4486
Chris Belanger <belanger.chris@gmail.com>
2017-01-22 15:04:02 -0500

    Initial Pocos

---
+++ Data/Entities/Substitution.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RedSquirrel.Data.Entities
+{
+    public class Substitution
+    {
+       public Substitution()
+       {
+           throw new NotImplementedException();
+       }
+    }
+}
---
+++ Data/Entities/Unit.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace RedSquirrel.Data.Entities
+{
+    public class Unit
+    {
+        public Int32  Id { get; set;}
+        public String Name { get; set; }
+    }
+}
