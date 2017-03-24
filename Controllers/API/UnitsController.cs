ac1166d2f9caa8edd26fe446bd4d4f328089496a
Christopher Belanger <belanger.chris@gmail.com>
2017-03-23 23:09:23 -0400

    bad req and log

--- Controllers/API/UnitsController.cs
+++ Controllers/API/UnitsController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using RedSquirrel.Models;
 using RedSquirrel.Services;
-using Microsoft.AspNetCore.Cors;  
+using Microsoft.AspNetCore.Cors;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Logging;
 
 namespace RedSquirrel.Controllers.API
 {
@@ -11,10 +13,12 @@ namespace RedSquirrel.Controllers.API
     public class UnitsController : Controller
     {
         protected UnitService Service { get; set; }
+        private ILogger<UnitService> Log { get; }
 
-        public UnitsController(UnitService service)
+        public UnitsController(UnitService service, ILogger<UnitService> log)
         {
             Service = service;
+            Log = log;
         }
 
         // GET api/values
@@ -35,6 +39,12 @@ namespace RedSquirrel.Controllers.API
         [HttpPost]
         public ActionResult  Post(Unit value)
         {
+            if (value == null)
+            {
+                return new BadRequestResult();
+            }
+
+            Log.LogDebug("Adding new unit. Name:" + value.Name );
             var id = Service.AddUnit(value);
             return new CreatedResult(id.ToString(), value);
         }
