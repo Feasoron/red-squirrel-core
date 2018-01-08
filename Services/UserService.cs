124da42fc82222de122cd41c0773d7dfa115a221
Chris Belanger <chris@chrisbelanger.net>
2018-01-08 14:47:20 -0500

    use automapper

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,35 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using RedSquirrel.Data;
+using RedSquirrel.Models;
 
 namespace RedSquirrel.Services
 {
     public class UserService : DataBackedService
     {
+        // Todo - this isn't a great cache. Add redis eventually
+        private static Dictionary<String, Int64> ExternalIdMap { get; } = new Dictionary<String, Int64>();
+        
         public UserService(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
         {
         }
 
-        public Int64 GetOrCreateUserId(Models.User user)
+        public Int64 GetOrCreateUserId(User user)
         {
             var dbUser = Context.Users.FirstOrDefault(u => u.ExternalUserId == user.ExternalUserId);
-
-            if (dbUser == null)
+            
+            if (dbUser != null)
             {
-                // TODO - Move to automapper
-                dbUser = new Data.Entities.User()
-                {
-                    Email = user.Email,
-                    Name = user.Name,
-                    ExternalUserId = user.ExternalUserId
-                };
-
-                Context.Users.Add(dbUser);
-                Context.SaveChanges();
+                return dbUser.UserId;
             }
 
+            dbUser = Mapper.Map<Data.Entities.User>(user);
+
+            Context.Users.Add(dbUser);
+            Context.SaveChanges();
+
             return dbUser.UserId;
+        }
+
+        public Int64 GetUserId(String externalId)
+        {
+            if (ExternalIdMap.ContainsKey(externalId))
+            {
+                return ExternalIdMap[externalId];
+            }
+            
+            var id = Context.Users.First(u => u.ExternalUserId == externalId).UserId;
+            ExternalIdMap.Add(externalId, id);
+
+            return id;
         }
     }
 }
